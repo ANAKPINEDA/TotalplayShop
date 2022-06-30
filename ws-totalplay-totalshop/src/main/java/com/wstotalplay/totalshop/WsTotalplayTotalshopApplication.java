fcee711c1dda757be9577cb69ac04fad1c671622
Marco <marco.castrot@totalplay.com.mx>
2022-06-30 13:31:28 -0500

    Configuración inicial

--- ws-totalplay-totalshop/src/main/java/com/wstotalplay/totalshop/WsTotalplayTotalshopApplication.java
+++ ws-totalplay-totalshop/src/main/java/com/wstotalplay/totalshop/WsTotalplayTotalshopApplication.java
@@ -8,6 +8,9 @@ public class WsTotalplayTotalshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsTotalplayTotalshopApplication.class, args);
+		System.out.println("==============================================================");
+		System.out.println("I N I C I A N D O     A P L I C A C I O N");
+		System.out.println("==============================================================");
 	}
 
 }
